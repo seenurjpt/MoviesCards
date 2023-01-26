78cd55fbf7c92e48aab6f8e801c2a5306b4de99d
Rajput Sunny <84331732+seenurjpt@users.noreply.github.com>
2023-01-26 05:58:21 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import Moviecard from "./Moviecard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=485c72dd";
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("Golmaal");
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  const searchMovies = async () => {
+    const response = await fetch(`${API_URL}&s=${searchTerm}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movies Lelo</h1>
+      <div className="search">
+        <input
+          placeholder="Search Movie here"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="\O/"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <Moviecard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Opps!! No Movies Found.. </h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+// copyright &copy; seenutodolist.com
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="footer-distributed">
+        <div class="footer-left">
+          <h3>
+            MoviesLelo<span> by Seenu</span>
+          </h3>
+          <p class="footer-company-name">copyright &copy; movieslelo.com</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
