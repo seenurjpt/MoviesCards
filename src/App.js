f691d684b7d3f5324e24a3b5508b39d715664684
seenurjpt <seenurjpt1402@gmail.com>
2023-06-10 12:34:45 +0530

    Updated 10.06(1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Moviecard from "./Moviecard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=485c72dd";
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState("Golmaal");
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    searchMovies();
-  }, []);
 
-  const searchMovies = async () => {
-    const response = await fetch(`${API_URL}&s=${searchTerm}`);
-    const data = await response.json();
-    setMovies(data.Search);
-    console.log(data);
-  };
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+const searchItem = ()=>{
+axios.get(`http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}&s=${searchTerm}`).then((result)=>{
+  setMovies(result.data.Search)
+  console.log("help",result.data);
+})
+}
+useEffect(() => {
+  searchItem()
+}, []);
   return (
     <div className="app">
       <h1>Movies Cards</h1>
@@ -25,15 +26,14 @@ const App = () => {
         <input
           placeholder="Search Movie here"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={(e) => {setSearchTerm(e.target.value)}}
         />
         <img
           src={SearchIcon}
           alt="\O/"
-          onClick={() => searchMovies(searchTerm)}
+          onClick={() => searchItem(searchTerm)}
         />
       </div>
-
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Footer from "./Footer";
-
-// ReactDOM.render(<App/>,  document.getElementById('root'));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Footer/>
   </React.StrictMode>
 )
